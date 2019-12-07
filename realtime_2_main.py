5672d7ded9ae114895baecc3099a6830012c93f3
ShimaP <53396572+Ronald-cons@users.noreply.github.com>
2019-12-08 06:29:47 +0800

    Update realtime_2_main.py

--- realtime_2_main.py
+++ realtime_2_main.py
@@ -12,7 +12,6 @@ from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 import scipy.signal as sig
 from pyfirmata2 import Arduino
-from scipy import integrate
 import iir_filter_complete
 import multiprocessing
 import pandas as pd
@@ -59,7 +58,7 @@ qtPanningPlot2 = QtPanningPlot("Arduino 2nd channel")
 
 # sampling rate: 100Hz
 samplingRate = 100
-
+count: int = 0
 #do filter here-----------------------------------------
 myFilter = iir_filter_complete.IIR(2, [1, 8], 'bandpass', design='butter')
 # called for every new sample at channel 0 which has arrived from the Arduino
@@ -68,6 +67,7 @@ def callBack(data):
     # filter your channel 0 samples here:
     # data = self.filter_of_channel0.dofilter(data)
     # send the sample to the plotwindow
+    threshold = 0.1
     qtPanningPlot1.addData(data)
     ch1 = board.analog[0].read()
     # 1st sample of 2nd channel might arrive later so need to check
@@ -75,18 +75,13 @@ def callBack(data):
         # filter your channel 1 samples here:
         ch1 = myFilter.doFilter(ch1)
         qtPanningPlot2.addData(ch1)
-        vol = volume_count(ch1)
-        print('Total mL ',vol)
-        if vol > 500:
-            print('Stop!Halt!Bitte sparen Sie Ressourcen!')
+        global count
+        if (ch1>threshold):
+            count += 1
+        print('Total mL ',count)
 
-def volume_count(data):
-    threshold = 0.1
-    count = 0
-    #data = board.analog[0].read()
-    if data > threshold:
-        count += 1
-    return count
+        if count > 500:
+            print('Stop!Halt!Bitte sparen Sie Ressourcen!')
 
 # Get the Ardunio board.
 board = Arduino(PORT)

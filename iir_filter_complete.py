f3e05da4b74bc9711908c11953234af582d79e89
ShimaP <53396572+Ronald-cons@users.noreply.github.com>
2019-12-10 05:33:01 +0800

    Update iir_filter_complete.py
    reset the filter parameters, add comments

--- iir_filter_complete.py
+++ iir_filter_complete.py
@@ -2,45 +2,63 @@ import scipy.signal as signal
 import numpy as np
 
 
-
-fs = 100
-
 class IIR(object):
     """
-    Given the proper parameters, this class calculates a filter (Butterworth, Chebyshev1 or Chebyshev2) and process an
-    input value from the reading.
+    At the instantiation of the filter the following parameters are compulsory:
     Attributes:
-        @:param order: Can be odd or even order as this class creates an IIR filter through the chain of second order filters and
-        an extra first order at the end if odd order is required.
-        @:param cutoff: For Lowpass and Highpass filters only one cutoff frequency is required while for Bandpass and Bandstop
-        it is required an array of frequencies. The input values must be float or integer and the class will
-        normalise them to the Nyquist frequency.
-        @:param filterType: lowpass, highpass, bandpass, bandstop
-        @:param design: butter, cheby1, cheby2.
-        @:param rp: Only for cheby1, it defines the maximum allowed passband ripples in decibels.
-        @:param rs: Only for cheby2, it defines the minimum required stopband attenuation in decibels.
+    Comulsory:
+        @:param order: positive integer
+            Can be odd or even order as this class creates a chain of second
+            order filters and an extra first order one if necessary.
+        @:param cutoff: array/positive float
+            Depending on the desired filter 1 cutoff frequency is to be
+            enetered as a positive float for low/highpass filters or
+            2 cutoff frequenices to be entered in an array as positive floats
+            for bandstop and bandpass filters. These cutoff frequencies can be
+            either entered as normalised to the Nyquist frequency (1 =
+            Nyquist frequency) or as Hz (0 < f < Nyquist), but in this case fs,
+            the sampling frequency has to be passed too.
+        @:param filterType: string
+            lowpass, highpass, bandpass, bandstop
+    Non-compulsory:
+        @:param design:
+            Different types of coefficient generations
+            can be chosen. The three available filters are Butterworth,
+            Chebyshev type 1 or type 2.
+            butter, cheby1, cheby2.
+            If left unspecified the default value is butter.
+        @:param rp: positive float
+            Only for cheby1, it defines the maximum allowed passband ripples in decibels.
+            If unspecified the default is 1.
+        @:param rs: positive float
+            Only for cheby2, it defines the minimum required stopband attenuation in decibels.
+            If unspecified the default is 1.
+
         For the filtering of the signal, an IIR filter was implemented in the class IIR:
         IIR(order,cutoff,filterType,design='butter',rp=1,rs=1,fs=0)
         This class calculate the IIR filter coefficients for a given filter type ( Butter, Cheby1 or Cheby2)
         and then, the class function "filter" does the filtering to a given value.
     """
-    def __init__(self, order, cutoff, filterType, design='butter', rp=1, rs=1):
+    def __init__(self, order, cutoff, filterType, design='butter', rp=1, rs=1,fs=0):
         for i in range(len(cutoff)):
             cutoff[i] = cutoff[i] / fs * 2
         if design == 'butter':
-            self.coefficients = signal.butter(order, cutoff, filterType, output='sos')
+            self.coeff = signal.butter(order, cutoff, filterType, output='sos')
         elif design == 'cheby1':
-            self.coefficients = signal.cheby1(order, rp, cutoff, filterType, output='sos')
+            self.coeff = signal.cheby1(order, rp, cutoff, filterType, output='sos')
         elif design == 'cheby2':
-            self.coefficients = signal.cheby2(order, rs, cutoff, filterType, output='sos')
-        self.acc_input = np.zeros(len(self.coefficients))
-        self.acc_output = np.zeros(len(self.coefficients))
-        self.buffer1 = np.zeros(len(self.coefficients))
-        self.buffer2 = np.zeros(len(self.coefficients))
+            self.coeff = signal.cheby2(order, rs, cutoff, filterType, output='sos')
+        else:
+            self.error = 0
+        self.acc_input = np.zeros(len(self.coeff))
+        self.acc_output = np.zeros(len(self.coeff))
+        self.buffer1 = np.zeros(len(self.coeff))
+        self.buffer2 = np.zeros(len(self.coeff))
         self.input = 0
         self.output = 0
+        self.error = 1
 
-    def filter(self, input):
+    def doFilter(self, input):
         """
         From the coefficients calculated in the constructor of the class, the filter is created as chains of IIR filters
         to obtain any order IIR filter. This is important as if order 8 IIR filter is required, it can be calculated as
@@ -56,14 +74,14 @@ class IIR(object):
         If it is needed a 8th order filter, the loop will be executed 4 times obtaining
         a chain of 4 2nd order filters.
         """
-        for i in range(len(self.coefficients)):
-            self.FIRcoeff = self.coefficients[i][0:3]
-            self.IIRcoeff = self.coefficients[i][3:6]
+        for i in range(len(self.coeff)):
+            self.FIRcoeff = self.coeff[i][0:3]
+            self.IIRcoeff = self.coeff[i][3:6]
 
             """
             IIR Part of the filter:
             The accumulated input are the values of the IIR coefficients multiplied
-            by the variables of the filter: the input and the delay lines.
+            by the variables of the filter:delay buffers weighed by the IIR coefficients.
             """
             self.acc_input[i] = (self.input + self.buffer1[i]
                                  * -self.IIRcoeff[1] + self.buffer2[i] * -self.IIRcoeff[2])
@@ -71,7 +89,8 @@ class IIR(object):
             """
             FIR Part of the filter:
             The accumulated output are the values of the FIR coefficients multiplied
-            by the variables of the filter: the input and the delay lines.
+            by the variables of the filter: input and the values from the delay bufferes 
+            weighed by the FIR coefficients.
             """
             self.acc_output[i] = (self.acc_input[i] * self.FIRcoeff[0]
                                   + self.buffer1[i] * self.FIRcoeff[1] + self.buffer2[i]
@@ -84,4 +103,3 @@ class IIR(object):
 
         self.output = self.acc_output[i]
         return self.output
-
